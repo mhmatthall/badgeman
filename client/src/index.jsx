5bf29a723bdf11ab9ba3d3e1e074832a90e86892
Matt <42594962+mhmatthall@users.noreply.github.com>
2022-09-17 23:50:20 +0100

    Fix typo in file naming

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 
 import './index.css';
-import Login from './routes/Login';
+import Login from './routes/login';
 
 const router = createBrowserRouter([
   {
--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Login.scss'
+import './login.scss'
 
 class Login extends React.Component {
   render() {
