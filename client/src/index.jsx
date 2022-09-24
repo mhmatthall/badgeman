0fd79e9dbc1d06000b8976e5b875300e6a8bee9c
Matt <42594962+mhmatthall@users.noreply.github.com>
2022-09-24 22:06:13 +0100

    Refactor config file usage on client site to avoid using disallowed browser import 'fs' (moved file reading to preval block)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import fs from "fs";
-import ini from "ini";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Import client files
@@ -10,10 +8,19 @@ import Login from "./routes/login";
 import Editor from "./routes/editor";
 import ErrorPage from "./routes/error";
 
+// Import babel macro for preval
+import preval from "babel-plugin-preval/macro";
+
 // Import INI file from project root
-export const config = ini.parse(
-  fs.readFileSync(__dirname + "/../../../config.ini", "utf8")
-);
+// Requires preval execution via Node because 'fs' disallowed on browser
+const configLoader = preval`
+  const fs = require("fs");
+  const ini = require("ini");
+  module.exports = ini.parse(
+    fs.readFileSync(__dirname + "/../../config.ini", "utf8")
+  );
+`;
+export const config = configLoader;
 
 const router = createBrowserRouter([
   {
